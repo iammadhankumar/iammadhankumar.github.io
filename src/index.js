d0cc85fa8fbcbf2c090ba232b76032efaca46145
Madhan Kumar <madhansmk1697@gmail.com>
2025-06-01 17:25:29 +0530

    deploy

--- src/index.js
+++ src/index.js
@@ -5,6 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './components/ThemeContext'; 
 
+// index.js (top of file, before ReactDOM.createRoot)
+if (sessionStorage.redirect) {
+  const redirect = sessionStorage.redirect;
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, null, redirect);
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -13,6 +21,7 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
