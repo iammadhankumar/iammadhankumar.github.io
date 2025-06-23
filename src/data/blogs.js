ecc28755180eac5e910f29daa2a6223d40ddf892
Madhan Kumar <madhansmk1697@gmail.com>
2025-06-23 23:25:53 +0530

    blog add

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -6,7 +6,7 @@ const blogPosts = [
     description: 'Unlocking the power of OpenAI and Hugging Face in your Java applications',
     url: 'https://medium.com/javarevisited/how-to-connect-java-with-generative-ai-c3df715391a8',
   },
-    {
+  {
     title: 'Interesting Java Facts You Probably Did not Know',
     date: 'June 08, 2025',
     readTime: '4 min read',
@@ -20,12 +20,19 @@ const blogPosts = [
     description: 'Mastering Design Patterns to Solve Real Development Challenges',
     url: 'https://dev.to/iammadhankumar/java-design-patterns-in-real-projects-2fmf',
   },
-    {
+  {
     title: 'Java Features in 2025',
     date: 'June 22, 2025',
     readTime: '5 min read',
     description: 'A Comprehensive Overview Welcome to the Future of Java',
     url: 'https://dev.to/iammadhankumar/java-features-in-2025-479m',
+  },
+  {
+    title: 'Java Developer Skills Checklist in 2025',
+    date: 'June 23, 2025',
+    readTime: '3 min read',
+    description: 'Explore the Must-Have Java Tech Stack That Makes You Stand Out',
+    url: 'https://dev.to/iammadhankumar/java-developer-skills-checklist-in-2025-9i4',
   },
   // Add more blog objects with readTime...
 ];
